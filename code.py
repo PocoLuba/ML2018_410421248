c16da1611c739c4daf0b91f1daae848a9db4220b
PocoLuba <410421248@gms.ndhu.edu.tw>
2018-05-05 19:20:28 +0800

    test

---
+++ code.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import random
+img_k1 = Image.open('key1.png')
+img_k2 = Image.open('key2.png')
+img_E = Image.open('E.png')
+img_Eprime = Image.open('Eprime.png')
+
+Limit = 50
+Epoch = 1
+
+w = [1,1,1]
+
+while Epoch == 1 or  Epoch < Limit:
+    for i in range(0,400):
+        for j in range(0,300):
+            x
+            a[i,j] = 
